79fbdff0c48d93a74fee607cf8811dcb9f5bc46f
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-04-13 18:27:10 +0430

    added deleting todo item features

--- src/scripts/new_todo.js
+++ src/scripts/new_todo.js
@@ -4,6 +4,13 @@ const add_todo_input = document.getElementById('add-todo-input');
 const add_todo_checkbox = document.getElementById('add-todo-checkbox');
 const todo_list = document.getElementById('todo-list');
 
+const auto_todo_close_btn_1 = document.getElementById('auto-todo-close-btn-1');
+const auto_todo_close_btn_2 = document.getElementById('auto-todo-close-btn-2');
+const auto_todo_close_btn_3 = document.getElementById('auto-todo-close-btn-3');
+const auto_todo_close_btn_4 = document.getElementById('auto-todo-close-btn-4');
+const auto_todo_close_btn_5 = document.getElementById('auto-todo-close-btn-5');
+const auto_todo_close_btn_6 = document.getElementById('auto-todo-close-btn-6');
+
 // A Function That Creates Todo Item
 function add_todo(name) {
     // If Name Of Todo Was Empty Show A Toast Otherwise Add Task.
@@ -24,6 +31,7 @@ function add_todo(name) {
         input.className = 'todo-input';
         todo_div.className = 'todo-holder--todo--list--item';
         input.value = name;
+        delete_btn.addEventListener('click', () => {list_element.remove();})
 
         // If New To Do Checkbox Is Checked Then Create Checked Checkbox.
         if (add_todo_checkbox.checked) {checkbox.setAttribute('checked', 'true');}
@@ -49,3 +57,11 @@ function add_todo(name) {
 add_todo_input.addEventListener('keydown', (key) => {
     if (key.key === 'Enter') {add_todo(add_todo_input.value);}
 })
+
+// Adding event Listener On Auto Generated Todos , Close Buttons That Listens To Click And Removes That Todo.
+auto_todo_close_btn_1.addEventListener('click', () => auto_todo_close_btn_1.parentElement.parentElement.remove());
+auto_todo_close_btn_2.addEventListener('click', () => auto_todo_close_btn_2.parentElement.parentElement.remove());
+auto_todo_close_btn_3.addEventListener('click', () => auto_todo_close_btn_3.parentElement.parentElement.remove());
+auto_todo_close_btn_4.addEventListener('click', () => auto_todo_close_btn_4.parentElement.parentElement.remove());
+auto_todo_close_btn_5.addEventListener('click', () => auto_todo_close_btn_5.parentElement.parentElement.remove());
+auto_todo_close_btn_6.addEventListener('click', () => auto_todo_close_btn_6.parentElement.parentElement.remove());
